c11b15d993357116082d81bdb81c6d4cc34f9599
Falkkor <natura.vigil@gmail.com>
2020-08-01 01:28:52 -0700

    init new format

---
+++ README.md
@@ -0,0 +1,36 @@
+# A Discord Music Bot written in JavaScript, the discord.js library and discord.js-commando framework
+
+### Installing the dependencies
+
+`npm i`
+
+### Setup
+
+Make a config.json file in the root directory of the project and add:
+
+```
+{
+  "prefix": "!",
+  "discord_owner_id": "Your-Discord-ID",
+  "token": "Your-Bot-Token",
+  "tenorAPI": "tenor-API-key",
+  "newsAPI": "news-api-key",
+  "youtubeAPI": "youtube-api-key",
+  "yandexAPI": "yandex-api-key",
+  "geniusLyricsAPI": "genius-api-key"
+}
+```
+
+Also, make sure [ffmpeg](https://www.ffmpeg.org/download.html) and [python 2.7](https://www.python.org/downloads/) are installed.
+ **Discord.js now requires Node version greater than or equal to 12.0.0** .
+
+
+### Resources
+
+[How to get a Youtube API key](https://developers.google.com/youtube/v3/getting-started)
+
+[Get a Genius API key here](https://genius.com/api-clients/new)
+
+[Installing node.js on Windows](https://treehouse.github.io/installation-guides/windows/node-windows.html)
+
+
---
+++ index.js
@@ -0,0 +1,98 @@
+const { CommandoClient } = require('discord.js-commando');
+const { Structures } = require('discord.js');
+const path = require('path');
+const { prefix, token, discord_owner_id } = require('./config.json');
+
+Structures.extend('Guild', function (Guild) {
+  class MusicGuild extends Guild {
+    constructor(client, data) {
+      super(client, data);
+      this.musicData = {
+        queue: [],
+        isPlaying: false,
+        nowPlaying: null,
+        songDispatcher: null,
+        volume: 1
+      };
+      this.triviaData = {
+        isTriviaRunning: false,
+        wasTriviaEndCalled: false,
+        triviaQueue: [],
+        triviaScore: new Map()
+      };
+    }
+  }
+  return MusicGuild;
+});
+
+const client = new CommandoClient({
+  commandPrefix: prefix,
+  owner: discord_owner_id // value comes from config.json
+});
+
+client.registry
+  .registerDefaultTypes()
+  .registerGroups([
+    ['music', 'Music Command Group'],
+    ['other', 'random types of commands group'],
+    ['guild', 'guild related commands']
+  ])
+  .registerDefaultGroups()
+  .registerDefaultCommands({
+    eval: false,
+    prefix: false,
+    commandState: false
+  })
+  .registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.once('ready', () => {
+  console.log('Ready!');
+  client.user.setActivity("with users");
+  const channel = client.channels.cache.find(ch => ch.name === 'bot-logs');
+  channel.send(`DDiscordBot is online.`);
+});
+
+client.on('voiceStateUpdate', async (___, newState) => {
+  if (
+    newState.member.user.bot &&
+    !newState.channelID &&
+    newState.guild.musicData.songDispatcher &&
+    newState.member.user.id == client.user.id
+  ) {
+    newState.guild.musicData.queue.length = 0;
+    newState.guild.musicData.songDispatcher.end();
+    return;
+  }
+  if (
+    newState.member.user.bot &&
+    newState.channelID &&
+    newState.member.user.id == client.user.id &&
+    !newState.selfDeaf
+  ) {
+    newState.setSelfDeaf(true);
+  }
+});
+
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
+  if (!channel) return;
+  channel.send(`Welcome ${member}!`);
+});
+
+client.on("messageDelete", (messageDelete) => {
+
+  const channel = client.channels.cache.find(ch => ch.name === 'bot-logs');
+  channel.send(` Channel: \`${messageDelete.channel.name}\`, Message : \`${messageDelete.content}\`, By \`${messageDelete.author.username}\` was deleted.`);
+
+});
+
+client.on('message', async message => {
+  if (message.author.bot) return;
+  else if (!message.content.startsWith(prefix)) return;
+  else {
+    const channel = client.channels.cache.find(ch => ch.name === 'bot-logs');
+    channel.send(`${message.author.username} issued command \`${message}\`.`);
+  }
+});
+
+client.login(token);
